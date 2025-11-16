033712ff88d4daf78ae94b54d23ec3713fa730fb
Seojunhyeong2003 <seojunhyeong2003@gmail.com>
2025-11-16 16:47:37 +0900

    - 폰트, UI개발중

---
+++ app/layouts/main-header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="h-16 sticky top-0 z-50 backdrop-blur-lg bg-opacity-50">
+        <nav class="px-7 h-full max-w-6xl mx-auto flex justify-between items-center">
+            <p class="font-cafe24 text-2xl">
+                밸런곰
+            </p>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+
+</script>
